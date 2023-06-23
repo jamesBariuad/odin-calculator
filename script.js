473a876d544ef9e3b582ed85b9ecd2db79432076
jokandro <114486039+jokandro@users.noreply.github.com>
2023-06-23 14:25:49 +0800

    feat: Correct Output When Pressing Equals Key

--- script.js
+++ script.js
@@ -32,33 +32,103 @@ const operate = (firstNumber, secondNumber, operator) => {
   }
 };
 
-let displayValue = "";
+let operatorIsclicked = false;
 
 const display = document.querySelector("#display");
-
 const digits = document.querySelectorAll(".digits");
+
+const isOperatorClicked = () => {
+  if (operatorIsclicked == true && display.textContent != "") {
+    storeCurrentDisplay();
+    display.textContent = "";
+    operatorIsclicked = false;
+  }
+};
+
+const displayDigitsClicked = (e) => {
+  isOperatorClicked();
+  if (display.textContent == 0) {
+    display.textContent = "";
+  }
+
+  display.textContent += e.target.textContent;
+};
+
 digits.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    displayValue += e.target.textContent;
-    display.textContent = displayValue;
-  });
+  button.addEventListener("click", displayDigitsClicked);
 });
 
-// const operators = document.querySelectorAll(".operators")
-// operators.forEach((button)=>{
-//     button.addEventListener("click", (e)=>{
-//         firstNumber = +display.textContent
-//         operator=e.target.id
-//         console.log(operator,firstNumber)
-//     })
-// })
+let result = undefined;
 
-const ac= document.querySelector("#ac")
-ac.addEventListener("click",()=>{
-    operator=""
-    firstNumber=""
-    secondNumber=""
-    displayValue=""
-    display.textContent=0
-})
+const storeCurrentDisplay = () => {
+  if (!firstNumber) {
+    return (firstNumber = +display.textContent);
+  }
+  if (firstNumber && !secondNumber) {
+    return (secondNumber = +display.textContent);
+  }
+};
+const operators = document.querySelectorAll(".operators");
 
+const handleOperatorClick = (e) => {
+  operatorIsclicked = true;
+  if (!operator) {
+    operator = e.target.id;
+  }
+  if (operator && e.target.classList[1] == "operators") {
+    operator = e.target.id;
+  }
+};
+operators.forEach((button) =>
+  button.addEventListener("click", (e) => handleOperatorClick(e))
+);
+
+const ac = document.querySelector("#ac");
+const clearAll = () => {
+  operator = undefined;
+  display.textContent = 0;
+  firstNumber = undefined;
+  secondNumber = undefined;
+  result = undefined;
+};
+ac.addEventListener("click", () => clearAll());
+
+const equals = document.querySelector("#equals");
+
+const handleEqualsClick = (e) => {
+  if (!firstNumber) {
+    return;
+  }
+  if (firstNumber && !secondNumber) {
+    storeCurrentDisplay();
+  }
+
+  if(result==+display.textContent){
+    firstNumber=result
+    result=operate(firstNumber,secondNumber,operator) 
+    return display.textContent= result
+  }
+  if (result) {
+    firstNumber = result;
+    secondNumber = +display.textContent;
+    console.log(firstNumber, secondNumber, e.target.id);
+    result = operate(firstNumber, secondNumber, operator);
+    return (display.textContent = result);
+  }
+
+  if ((firstNumber, secondNumber, operator)) {
+    result = operate(firstNumber, secondNumber, operator);
+    return (display.textContent = result);
+  }
+};
+equals.addEventListener("click", (e) => handleEqualsClick(e));
+
+const buttons = document.querySelectorAll(".buttons");
+buttons.forEach((button) => {
+  button.addEventListener("mousedown", () => {
+    button.style.backgroundColor = "black";
+  });
+  button.addEventListener("mouseup", () => {
+    button.style.backgroundColor = "peachpuff";
+  });
+});
